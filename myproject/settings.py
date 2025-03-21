531f54136663d3c0ed3bca3b216c8f1b60ccb998
Sidharth <sunilsidharth411@gmail.com>
2025-03-21 12:50:10 +0530

    added deployment settings4

--- myproject/settings.py
+++ myproject/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 BASE_DIR=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = 'static/'
 STATIC_ROOT=os.path.join(BASE_DIR,"staticfiles")
-
+STATICFILES_DIRS = [BASE_DIR / "static"] 
 
 MEDIA_URL = "/images/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
